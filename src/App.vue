6da8fd8eaac0f1beae1ae92af25be3f03d4c5492
Bryce Fountain <me@skoll.dev>
2023-10-03 20:22:28 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app">
+    <Sidebar />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import Sidebar from './components/Sidebar/SidebarMenu.vue'
+</script>
+
+<style lang="scss">
+:root {
+  --primary: #d2af39;
+  --primary-alt: #b6860a;
+  --grey: #64748b;
+  --dark: #0e1d3a;
+  --dark-alt: #001947;
+  --light: #ffffff;
+  --sidebar-width: 300px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira sans', sans-serif;
+}
+
+body {
+  background: var(--light);
+}
+
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+.app {
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+
+    @media (max-width: 1024px) {
+      padding-left: 6rem;
+    }
+  }
+}
+</style>
